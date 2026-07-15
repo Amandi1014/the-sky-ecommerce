c995c08051d5e84f0c1a508cfc75e8231e5de82c
Amandi Sithmini <rasangikasithmini@gmail.com>
2026-07-15 17:01:02 +0530

    Add Category create feature - Entity, Repository, Service, Controller

---
+++ src/main/java/com/thesky/CategoryController.java
@@ -0,0 +1,21 @@
+package com.thesky;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category) {
+        return categoryService.saveCategory(category);
+    }
+
+}
