f647d30f4f40ee78c52c7f554e67144104b72504
Amandi Sithmini <rasangikasithmini@gmail.com>
2026-07-16 09:57:31 +0530

    Add Category read feature - get all and get by id

---
+++ src/main/java/com/thesky/CategoryController.java
@@ -0,0 +1,29 @@
+package com.thesky;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Integer id) {
+        Optional<Category> category = categoryService.getCategoryById(id);
+        return category.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+}
---
+++ src/main/java/com/thesky/CategoryService.java
@@ -0,0 +1,23 @@
+package com.thesky;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> getCategoryById(Integer id) {
+        return categoryRepository.findById(id);
+    }
+
+}
